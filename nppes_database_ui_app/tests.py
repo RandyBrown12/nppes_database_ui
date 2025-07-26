94c31f4883693312567d54a3f2e0ede82afbe061
Randy <randybrown9812@gmail.com>
2025-07-25 22:09:30 -0500

    Add some unittests to the project

--- nppes_database_ui_app/tests.py
+++ nppes_database_ui_app/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from bs4 import BeautifulSoup
 
 # Create your tests here.
+class NPPESDatabaseUI(TestCase):
+    """
+    Verify that the NPPES Database UI renders correctly.
+    This test checks the presence of specific elements in the HTML response.
+    """
+    def test_navbar_text(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        navbar_text = soup.find('div', id='navbar-text')
+        self.assertEqual(navbar_text.get_text(strip=True), 'NPPES Database UI')
+    
+    def test_form_text(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        form_text = soup.find('div', id='form-text')
+        self.assertEqual(form_text.get_text(strip=True), 'Search NPPES Database')
